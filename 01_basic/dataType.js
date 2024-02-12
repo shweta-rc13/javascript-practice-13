f4b86f700d9b3d67386913d40569d428987fab68
shweta chaurasiya <shwetachaurasiya1313@gmail.com>
2024-02-12 18:23:48 +0530

    datatypes

---
+++ 01_basic/dataType.js
@@ -0,0 +1,23 @@
+let name = "Shweta"
+let age = 23
+let isPass = true
+let lname
+let fname = null
+
+console.log(name);
+console.log(age);
+console.log(isPass);
+console.log(lname);
+console.log(fname);
+
+/*dataTypes : Primitive and Reference
+-------------Primitive are :----------------
+1) numbers
+2) string
+3) Boolean
+4) undefined                        (when console.log(typeOf(undefined)) it will return Object, so undefined is an undefined type)
+5) null                             (when console.log(typeOf(null)) it will return Object, so null is an object)
+6) symbol 
+
+
+*/
--- 01_basic/variables.js
+++ 01_basic/variables.js
@@ -2,13 +2,21 @@
 // let accountName = "Saurav"
 // var accountPassword = "12345678"
 
-// //accountID = 01;        reassign not allowed in const
+// //accountID = 01;       ( reassign not allowed in const)
 // accountName = "Shweta"
 // console.log(accountID);
 // console.table([accountName, accountPassword])
 
-{
-    let name = "SAWO"
-}
+
+/* let name = "SAWO"       ( ek bar let variable name diya toh wapas reassign krne ke liye let nhi likhna padta hai)
+ console.log(name)         (wapas let krk reassign kiya toh error deta hai)
 let name = "OWAS"
 console.log(name)
+
+let name = "SAWO"
+console.log(name)
+ name = "OWAS"
+console.log(name)*/
+
+/*var fname;
+console.log(fname) */   //(it will return undefined)  
