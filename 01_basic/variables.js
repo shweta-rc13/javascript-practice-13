8f2e31fa4423b38f829e2de19618a5eefcfc0e59
shweta chaurasiya <shwetachaurasiya1313@gmail.com>
2024-02-12 17:42:44 +0530

    javascript-practise

---
+++ 01_basic/variables.js
@@ -0,0 +1,14 @@
+// const accountID = 13;
+// let accountName = "Saurav"
+// var accountPassword = "12345678"
+
+// //accountID = 01;        reassign not allowed in const
+// accountName = "Shweta"
+// console.log(accountID);
+// console.table([accountName, accountPassword])
+
+{
+    let name = "SAWO"
+}
+let name = "OWAS"
+console.log(name)
