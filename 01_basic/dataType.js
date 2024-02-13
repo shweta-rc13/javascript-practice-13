bd4d61b20446c268d5875736e4cb91e639c53cb8
shweta chaurasiya <shwetachaurasiya1313@gmail.com>
2024-02-13 16:48:21 +0530

    dataType

--- 01_basic/dataType.js
+++ 01_basic/dataType.js
@@ -10,6 +10,14 @@ console.log(isPass);
 console.log(lname);
 console.log(fname);
 
+
+//Symbol
+const id = Symbol('123')
+const anotherId = Symbol('123')
+console.log(id == anotherId);
+//BigInt
+const bigNumber= 1234567890987654321n
+
 /*dataTypes : Primitive and Reference
 -------------Primitive are :----------------
 1) numbers
@@ -17,8 +25,34 @@ console.log(fname);
 3) Boolean
 4) undefined   (when console.log(typeOf(undefined)) it will return Object, so undefined is an undefined type)
 5) null        (when console.log(typeOf(null)) it will return Object, so null is an object)
-6) symbol 
+6) symbol
+7) BigInt 
 
+
+------------ Reference ------------
+1) Array
+2) Objects
+3) Functions
 
 */
+//Array
+const color = ["Blue", "Black", "Yellow", "White"];
+console.log(color);
+
+// Object
+let myObj = {
+    name: "Shweta",
+    age: 23
+}
+console.log(myObj);
+
+// Function
+
+const myFunction = function(a,b){
+    c = a + b;
+    console.log(c);
+}
+myFunction(2,3)
+
+
 
