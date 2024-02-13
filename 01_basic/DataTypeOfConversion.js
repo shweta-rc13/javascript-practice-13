31290b8c1dfaf86321ce06e226bb2775311027e2
shweta chaurasiya <shwetachaurasiya1313@gmail.com>
2024-02-13 07:33:26 +0530

    datatypeConversion

---
+++ 01_basic/DataTypeOfConversion.js
@@ -0,0 +1,35 @@
+//let score = "33"  
+//let score = "33abc"
+//let score = null
+//let score = undefined
+//let score = true
+//let score= false
+//let score = "Shweta"
+
+
+/*console.log(typeof score);
+console.log(typeof(score));
+
+let valueinnumber = Number(score);
+console.log(typeof(valueinnumber));
+console.log(valueinnumber)*/
+
+
+
+//let ispass = 1
+//let ispass = ""
+//let ispass = "Shweta"
+
+/*let boolispass = Boolean(ispass)
+console.log(boolispass);*/
+
+
+
+
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof(stringNumber));
+
+
