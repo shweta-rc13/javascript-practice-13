5d2428a89b23bf22d7acfd56c4a4332a963e3d6b
shweta chaurasiya <shwetachaurasiya1313@gmail.com>
2024-02-13 16:03:51 +0530

    comparison

---
+++ 01_basic/comparison.js
@@ -0,0 +1,15 @@
+console.log("2" > 1);      //true
+console.log("02" > 1);     //true
+
+/*console.log(null > 0);     //false
+console.log(null == 0);    // false
+console.log(null >= 0);   // True
+
+the reason is that an equality check == and
+comparison > <> = <= work differently.
+Comparison convert null to a number, treating it as 0.*/
+
+
+console.log(undefined == 0); //false
+console.log(undefined < 0);  // False
+console.log(undefined > 0);  //false
