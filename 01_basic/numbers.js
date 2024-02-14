962da28d33d8e9b24142a5cf69eb2e0bb92405b6
shweta-rc13 <shwetachaurasiya1313@gmail.com>
2024-02-15 00:40:45 +0530

    numbers And Math

--- 01_basic/numbers.js
+++ 01_basic/numbers.js
@@ -13,3 +13,17 @@ console.log(num.toPrecision(3));
 
 const hundreds = 10000000
 console.log(hundreds.toLocaleString('en-IN'));
+
+
+//=================MATHS============================
+console.log(Math);
+console.log(Math.abs(-3));
+console.log(Math.round(4.6));
+console.log(Math.ceil(4.1));
+console.log(Math.floor(5.9));
+console.log((Math.random()*10 )+ 1);
+
+
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
