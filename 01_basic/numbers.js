ce3a4947169692619521477ba3c15afffd67fe28
shweta-rc13 <shwetachaurasiya1313@gmail.com>
2024-02-14 17:49:59 +0530

    numbers

---
+++ 01_basic/numbers.js
@@ -0,0 +1,15 @@
+const score = 400
+console.log(score);
+
+
+const balance = new Number(1000)
+console.log(balance);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+
+const num = 23.8944
+console.log(num.toPrecision(3));
+
+const hundreds = 10000000
+console.log(hundreds.toLocaleString('en-IN'));
