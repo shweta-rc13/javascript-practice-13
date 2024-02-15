e430b6b3ba10e8a37b567fde8da901a13ab6765b
shweta-rc13 <shwetachaurasiya1313@gmail.com>
2024-02-15 17:07:33 +0530

    arrays

---
+++ 02_basic/arrays.js
@@ -0,0 +1,56 @@
+const myArray =  [1, 2, 3, 4]
+// console.log(myArray[2]);
+
+// const myArray1 = new Array(56,78,90,12)
+// console.log(myArray1);
+
+// myArray.push(6)
+// myArray.push(5)
+// myArray.pop()
+
+// myArray.unshift(12)
+// myArray.shift()
+// console.log(myArray);
+
+// console.log(myArray.includes(5));
+// console.log(myArray.indexOf(3));
+// console.log(myArray.indexOf(13));
+
+// const newArr = myArray.join()
+// console.log(newArr);
+
+//slice, splice------------------------------
+/*console.log("A ",myArray);
+
+const myn1 = myArray.slice(1,3)
+console.log(myn1);
+console.log("B ", myArray);
+
+const myn2 = myArray.splice(1,3)
+console.log("C ", myArray);
+console.log(myn2);*/
+
+const marvel_heros = ["thor", "Ironman", "Spiderman"]
+const dc_heros = ["Superman", "Flash", "Batman"]
+//marvel_heros.push(dc_heros)
+
+// console.log(marvel_heros);
+// console.log(marvel_heros[3][2]);
+
+// const all = marvel_heros.concat(dc_heros)
+// console.log(all);
+
+// const all_new = [ ...marvel_heros, ...dc_heros]
+// console.log(all_new);
+
+// const another_array = [1,2,3,[4,5,6], 7 ,[6,7,[4,5]]]
+// const real_another_array = another_array.flat(Infinity)
+// console.log(real_another_array);
+
+// console.log(Array.isArray("Shayad"));
+// console.log(Array.from("Shweta"));
+
+// let score1 = 100
+// let score2 = 200
+// let score3 = 300
+// console.log(Array.of(score1, score2, score3));
