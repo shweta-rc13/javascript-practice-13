4e1da9e80340311b7f03fa14095407ddf2f62118
shweta-rc13 <shwetachaurasiya1313@gmail.com>
2024-02-16 08:05:33 +0530

    objects

--- 02_basic/objects.js
+++ 02_basic/objects.js
@@ -1,4 +1,4 @@
-const mySym = Symbol("key1")
+/*const mySym = Symbol("key1")
 const jsuser = {
     name : "Shweta",
     age : 23,
@@ -27,4 +27,34 @@ console.log(jsuser.greeting());
 jsuser.greeting2 = function(){
     console.log(`Hello JS user, ${ this.name}`);
 }
-console.log(jsuser.greeting2());
+console.log(jsuser.greeting2());  */
+
+
+//const tinderuser = new Object()
+const tinderuser = {}
+tinderuser.id ="123abc"
+tinderuser.name = "sawo"
+tinderuser.isLoggedin = false
+//console.log(tinderuser);
+
+const regularuser = {
+    email:"sawo@gmail.com",
+    fullname: {
+        userfullname: {
+            firstname : "Sawo",
+            lastname :"YT"
+        }
+    }
+}
+console.log(regularuser.fullname.userfullname.firstname);
+
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "a", 4: "b"}
+const obj4 = {5: "a", 6: "b"}
+
+//const obj3 = { obj1, obj2}
+//const obj3 = Object.assign({}, obj1, obj2, obj4)
+const obj3 = {...obj1, ...obj2, ...obj4}
+
+//console.log(obj3);
