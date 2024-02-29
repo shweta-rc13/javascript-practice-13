42522518bce6ac45e8d1452fd6f73999afe935a6
shweta-rc13 <shwetachaurasiya1313@gmail.com>
2024-02-29 17:56:46 +0530

    js practice

---
+++ 06_project/BMI_Calculator/script.js
@@ -0,0 +1,23 @@
+const form = document.querySelector('form')
+
+// const height = parseInt(document.querySelector('#height').value)
+// const weight = parseInt(document.querySelector('#weight').value)
+
+form.addEventListener('submit', function(e){
+    e.preventDefault()
+
+    const height = parseInt(document.querySelector('#height').value);
+    const weight = parseInt(document.querySelector('#weight').value);
+    const result = document.querySelector('#result');
+
+    if (height === "" || height < 0 || isNaN(height)) {
+       result.innerHTML = `Please give valid Height ${height}` 
+    }else if (weight === "" || weight < 0 || isNaN(weight)) {
+        result.innerHTML = `Please give valid weight ${weight} `
+     } else {
+       const bmi =  (weight / ((height * height) / 10000)).toFixed(2)
+       result.innerHTML  = `<span>${bmi}</span>`
+     }
+
+
+});
